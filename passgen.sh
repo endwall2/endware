d30abb9b9323eb8c14ded965a2df6343577482b3
Endwall <endwall@protonmail.com>
2019-07-07 22:02:08 -0600

    add --rand to activate /dev/random instead of /dev/urandom

--- passgen.sh
+++ passgen.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#! /bin/sh
 #####################################################################################################################################
 # TITLE: passgen.sh
 # TYPE: BOURNE SHELL SCRIPT
 # AUTHOR: THE ENDWARE DEVELOPMENT TEAM
 # CREATION DATE: JUNE 21, 2016
-# VERSION: 0.04
+# VERSION: 0.05
 # BRANCH: GNU/LINUX
-# REVISION DATE: SEPTEMBER 20, 2016
-# COPYRIGHT: THE ENDWARE DEVELOPMENT TEAM, 2016
+# REVISION DATE: July 08, 2019
+# COPYRIGHT: THE ENDWARE DEVELOPMENT TEAM, 2016-2019
 #
 #
 # DESCRIPTION: Generates a random password output to screen or a text file
@@ -17,7 +17,7 @@
 #             - Added EULA+Instructions
 #
 ######################################################################################################################################
-# DEPENDENCIES: base64, head, /dev/urandom
+# DEPENDENCIES: base64, head, /dev/urandom, /dev/random,   haveged
 ######################################################################################################################################
 # INSTRUCTIONS:
 #
@@ -31,6 +31,33 @@
 #  $ passgen outfile.txt  
 #
 #
+#############################################################################################################################################################################
+#                                         ACKNOWLEDGEMENTS
+#############################################################################################################################################################################
+#  The Endware Development Team would like to acknowledge the work and efforts of OdiliTime, and SnakeDude who graciously hosted and promoted this software project.  
+#  Without their efforts and their wonderful website www.endchan.xyz, The Endware Suite would not exist in the public domain at all in any form. 
+#
+#  So thanks to OdiliTime, and to SnakeDude for inspiring this work and for hosting and promoting it. 
+#  
+#  The Endware Suite including Endwall,Endsets,Endlists,Endtools,Endloads and Endtube are named in honor of Endchan.
+#
+#  The Endware Suite is available for download at the following locations:
+#  https://gitgud.io/Endwall/ , https://github.com/endwall2/, https://www.endchan.xyz/os/, http://42xlyaqlurifvvtq.onion,
+#
+#  Thank you also to early beta testers including a@a, and to other contributors 
+#  as well as to the detractors who helped to critique this work and to ultimately improve it.  
+#  
+#  We also acknowledge paste.debian.net, ix.io, gitgud and github for their hosting services, 
+#  without which distribution would be limited, so thank you.
+#
+#  https://www.endchan.xyz, http://paste.debian.net, https://gitgud.io, https://github.com, http://ix.io  
+#
+#  We salute you! 
+#  
+#  In the end, may it all end well.
+#
+#  The Endware Development Team
+##############################################################################################################################################################################
 #############################################################################################################################################################################
 #                                         ACKNOWLEDGMENTS
 #############################################################################################################################################################################
@@ -66,10 +93,11 @@
 #  BEGINNING OF LICENSE AGREEMENT
 #  TITLE:  THE ENDWARE END USER LICENSE AGREEMENT (EULA) 
 #  CREATION DATE: MARCH 19, 2016
-#  VERSION: 1.15
-#  VERSION DATE: JULY 05, 2017
-#  COPYRIGHT: THE ENDWARE DEVELOPMENT TEAM, 2016-2017
-#      
+#  VERSION: 1.18
+#  VERSION DATE: JUNE 28, 2018
+#  COPYRIGHT: THE ENDWARE DEVELOPMENT TEAM, 2016-2018
+#  ALL RIGHTS RESERVED  
+#    
 #  WHAT CONSTITUTES "USE"? WHAT IS A "USER"?
 #  0) a) Use of this program means the ability to study, possess, run, copy, modify, publish, distribute and sell the code as included in all lines of this file,
 #        in text format or as a binary file constituting this particular program or its compiled binary machine code form, as well as the the performance 
@@ -79,17 +107,18 @@
 #  1) a) This program may be used by any living human being, any person, any corporation, any company, and by any sentient individual with the willingness and ability to do so.
 #  1) b) This program may be used by any citizen or resident of any country, and by any human being without citizenship or residency.
 #  1) c) This program may be used by any civilian, military officer, government agent, private citizen, government official, sovereign, monarch, head of state,
-#        dignitary, ambassador, legislator,congressional representative, member of parliament, senator, judicial official, judge, prosecutor, lawyer, 
-#        noble, commoner, clergy, laity, and generally all classes and ranks of people, persons, and human beings mentioned and those not mentioned.
-#  1) d) This program may be used by any human being of any gender, including men, women, and any other gender not mentioned.       
-#  1) e) This program may be used by anyone of any affiliation, political viewpoint, political affiliation, religious belief, religious affiliation, and by those of non-belief or non affiliation.
-#  1) f) This program may be used by any person of any race, ethnicity, identity, origin, genetic makeup, physical appearance, mental ability, and by those of any other physical 
+#        dignitary, ambassador, legislator,congressional representative, member of parliament, senator, judicial official, judge, prosecutor, lawyer, law enforcement officer, 
+#        police constable, noble, commoner, clergy, laity, and generally all classes and ranks of people, persons, and human beings mentioned and those not mentioned.
+#  1) d) This program may be used by any human being of any sex or gender, including men, women, or any other sex, or gender not mentioned.       
+#  1) e) This program may be used by any human being of any affiliation, political viewpoint, political affiliation, religious belief, religious affiliation, and by those of non-belief or non affiliation.
+#  1) f) This program may be used by any human being of any race, ethnicity, identity, origin, genetic makeup, physical appearance, mental ability, and by those of any other physical 
 #        or non physical characteristics of differentiation.
 #  1) g) This program may be used by any human being of any sexual orientation, including heterosexual, homosexual, bisexual, asexual, or any other sexual orientation not mentioned.
-#  1) h) This program may be used by anyone. 
+#  1) h) This program may be used by all business classes and business entities, including corporations, limited liability companies, sole proprietorships, partnerships, joint venture companies, private companies, publicly owned companies, and any other business class not specifically mentioned. 
+#  1) i) This program may be used by anyone. 
 #  WHERE MAY A USER USE THIS PROGRAM ?
 #  2) a) This program may be used in any country, in any geographic location of the planet Earth, in any marine or maritime environment, at sea, sub-sea, in a submarine, underground,
-#        in the air, in an airplane, dirigible, blimp, or balloon, and at any distance from the surface of the planet Earth, including in orbit about the Earth or the Moon,
+#        in the air, in an airplane, dirigible, blimp, or balloon, in a car, bus, motor vehicle, armored transport vehicle, and at any distance from the surface of the planet Earth, including in orbit about the Earth or the Moon,
 #        on a satellite orbiting about the Earth, the Moon, about any Solar System planet and its moons, on any space transport vehicle, and anywhere in the Solar System including the Moon, Mars, and all other Solar System planets not listed.  
 #  2) b) This program may be used in any residential, commercial, business, and governmental property or location and in all public and private spaces. 
 #  2) c) This program may be used anywhere.
@@ -98,14 +127,16 @@
 #      business use, commercial use, government use, non-governmental organization use, non-profit organization use, military use, civilian use, and generally any other use 
 #      not specifically mentioned.
 #  WHAT MAY A "USER" DO WITH THIS PROGRAM ?
-#  4) Any user of this program is granted the freedom to study the code.
-#  5) a) Any user of this program is granted the freedom to distribute, publish, and share the code with any neighbor of their choice electronically or by any other method of transmission. 
+#  4) Any user of this program is granted the freedom to read and study the code.
+#  5) a) Any user of this program is granted the freedom to distribute, publish, and share the code with any recipient of their choice electronically or by any other method of transmission. 
 #  5) b) The LICENCSE AGREEMENT, ACKNOWLEDGMENTS, Header and Instructions must remain attached to the code in their entirety when re-distributed.
-#  5) c) Any user of this program is granted the freedom to sell this software as distributed or to bundle it with other software or salable goods.
-#  6) a) Any user of this program is granted the freedom to modify and improve the code.
-#  6) b) When modified or improved, any user of this program is granted the freedom of re-distribution of their modified code if and only if the user attatchs the LICENSE AGREEMENT
+#  5) c) Any user of this program is granted the freedom to sell this software as distributed or to bundle it with other software or saleable goods.
+#  6) a) Any user of this program is granted the freedom to modify the code.
+#  6) b) When modified, any user of this program is granted the freedom of re-distribution of their modified code if and only if the user attatchs the LICENSE AGREEMENT
 #        in its entirety to their modified code before re-distribution.
-#  6) c) Any user of this software is granted the freedom to sell their modified copy of this software or to bundle their modified copy with other software or salable goods.
+#  6) c) Any user of this software is granted the freedom to sell their modified copy of this software or to bundle their modified copy with other software or saleable goods.
+#  6) d) Any modified code shall be sublicensed by the modifier and distributor only under the original terms of the Endware End User License Agreement as presented in this LICENSE AGREEMENT.
+#  6) e) Any user of this software agrees that any derivative works produced as a result of user modification will be sublicensed when re-distributed under the original terms of this LICENSE AGREEMENT exactly as presented.
 #  7) a) Any user of this program is granted the freedom to run this code on any computer of their choice.
 #  7) b) Any user of this program is granted the freedom to run as many simultaneous instances of this code, on as many computers as they are able to and desire, and for as long as they desire and are
 #        able to do so with any degree of simultaneity in use. 
@@ -114,14 +145,14 @@
 #  9) Any user of this program is not granted the freedom to arbitrarily procure a copyright on this software as presented, and agrees not to do so.
 #  10) Any user of this program is not granted the freedom to obtain or retain intellectual property rights on this software as presented and agrees not to do so.
 #  11) a) Any user of this program may use this software as part of a patented process, as a substitutable input into the process; however the user agrees not to attempt to patent this software as part of their patented process. 
-#      b) This software is a tool, like a hammer, and may be used in a process which applies for and gains a patent, as a substitutable input into the process;
+#  11) b) This software is a tool, like a hammer, and may be used in a process which applies for and gains a patent, as a substitutable input into the process;
 #         however the software tool itself may not be included in the patent or covered by the patent as a novel invention, and the user agrees not to do this and not to attempt to do this.
 #  WHO GRANTS THESE FREEDOMS ?
-#  12) The creators of this software are the original developer,"Endwall", and anyone listed as being a member of "The Endware Development Team", as well as ancillary contributors, and user modifiers and developers of the software. 
+#  12) The creators of this software are the original developer,"Endwall", and anyone listed as being a member of "The Endware Development Team" by "Endwall", as well as ancillary contributors, and user modifiers and developers of the software. 
 #  13) The aforementioned freedoms of use listed in sections 4),5),6),and 7) are granted by the creators of this software and the Endware Development Team to any qualifying user listed in section 1) and 
 #      comporting with any restrictions and qualifications mentioned in sections 2), 3), 8), 9), 10) and 11) of this LICENSE AGREEMENT.
 #  WHAT RELATIONSHIP DO THE USERS HAVE WITH THE CREATORS OF THE SOFTWARE ?
-#  14)  This software is distributed "as is" without any warranty and without any guaranty and the creators do not imply anything about its usefulness or efficacy.
+#  14)  This software is distributed "AS IS" without any warranty and without any guaranty and the creators do not imply anything about its usefulness or efficacy.
 #  15)  If the user suffers or sustains financial loss, informational loss, material loss, physical loss or data loss as a result of using, running, or modifying this software 
 #       the user agrees that they will hold the creators of this software, "The Endware Development Team", "Endwall", and the programmers involved in its creation, free from prosecution, 
 #       free from indemnity, and free from liability, and will not attempt to seek restitution, compensation, or payment for any such loss real or imagined.
@@ -134,17 +165,19 @@
 #  17)  If a user finds a significant flaw or makes a significant improvement to this software, please feel free to notify the original developers so that we may also
 #       include your user improvement in the next release; users are not obligated to do this, but we would enjoy this courtesy tremendously.
 #
-#  18)  Sections 0) a) 0) b) and 1) a) are sufficient for use; however sections 1) b) through 1) h) are presented to clarify 1 a) and to enforce non-discrimination and non-exclusion of use.  
+#  18)  Sections 0) a) 0) b) and 1) a) are sufficient for use; however sections 1) b) through 1) i) are presented to clarify 1 a) and to enforce non-discrimination and non-exclusion of use.  
 #       For example some people may choose to redefine the meaning of the words "person" "human being" or "sentient individual" to exclude certain types of people.
 #       This would be deemed unacceptable and is specifically rejected by the enumeration presented.  If the wording presented is problematic please contact us and suggest a change,
 #       and it will be taken into consideration.  
 #################################################################################################################################################################################
+
 ###################################################     BEGINNING OF PROGRAM        #############################################################################################
-version="0.04"
+version="0.05"
 branch="gnu/linux"
-rev_date="20/09/2016"
+rev_date="08/06/2019"
 len_switch="off"
 byte_switch="off"
+random_switch="off"
 syntax="check"
 nargs=$#
 
@@ -179,6 +212,7 @@ echo "USAGE: $ passgen --version            # output version statements"
 echo "USAGE: $ passgen --bytes n            # output a random password using n bytes of urandom"
 echo "USAGE: $ passgen --outfile file.txt   # output a random password to file.txt"
 echo "USAGE: $ passgen --bytes n --outfile file.txt   # output a random password using n bytes of urandom to file.txt"
+echo "USAGE: $ passgen --rand               # using /dev/random instead of /dev/urandom use haveged with this switch"
 echo " "
 echo "Try $ passgen --bytes 18 for a 24 character password"
 echo " "
@@ -189,9 +223,16 @@ fi
 if [ "$arg" == --version ] 
 then 
 echo "PASSGEN version: "$version" branch: "$branch" revised on: "$rev_date" "
-echo "Copyright: THE ENDWARE DEVELOPMENT TEAM, 2016 "
+echo "Copyright: THE ENDWARE DEVELOPMENT TEAM, 2016-2019 "
 shift
 exit 1 
+fi
+
+if [ "$arg" == --rand ]
+then
+random_switch="on"
+syntax="good"
+shift
 fi
 
 if [ "$arg" == --bytes ]
@@ -216,20 +257,27 @@ syntax="check"
 
 done
 
+if [ "$random_switch" == "on" ]
+then 
+   device="random"
+else 
+   device="urandom"
+fi
+
 if [ "$has_file" == "yes" ] 
 then 
  if [ "$has_byte" == "yes" ] 
  then 
- head -c "$bytes" /dev/urandom | base64 -i >> "$outfile"
+ head -c "$bytes" /dev/"$device" | base64 -i >> "$outfile"
  else
- head -n 3 /dev/urandom | base64 -i >> "$outfile"
+ head -n 3 /dev/"$device" | base64 -i >> "$outfile"
  fi
 else
  if [ "$has_byte" == "yes" ] 
  then 
- head -c "$bytes" /dev/urandom | base64 -i 
+ head -c "$bytes" /dev/"$device" | base64 -i 
  else
- head -n 3 /dev/urandom | base64 -i
+ head -n 3 /dev/"$device" | base64 -i
  fi
 fi
 
